70e658791372725e42b9a38b9c5262da9c9c18ce
rodrigo silva <rodsilvasoul@gmail.com>
2021-08-07 11:33:42 -0400

    refector: crete user address test unit

--- src/modules/accounts/useCases/create-user-phone/create-user-phone-unit.spec.ts
+++ src/modules/accounts/useCases/create-user-phone/create-user-phone-unit.spec.ts
@@ -10,7 +10,7 @@ import {
 } from '@shared/container/providers/validator/implementations';
 import { UserNotFoundError } from '@shared/errors/useCase';
 import { EmptyBodyError, InvalidParamError } from '@shared/errors/validator';
-import { badRequest } from '@shared/http';
+import { badRequest, serverError } from '@shared/http';
 import { right } from '@shared/utils';
 
 import { CreateUserPhoneController, CreateUserPhoneUseCase } from '.';
@@ -30,12 +30,18 @@ const user_mock = {
 };
 
 const user_phone_mock = {
-  id_user: user_mock.id,
   phone_number: 'any_phone_number',
   type: 'any_type',
 };
 
-describe('create or update user phone use case', () => {
+const http_request = {
+  body: user_phone_mock,
+  params: {
+    id: user_mock.id,
+  },
+};
+
+describe('create user phone : unit', () => {
   beforeEach(() => {
     userRepository = new UserRepositoryInMemory();
     userPhoneRepository = new UserPhoneInMemory();
@@ -56,6 +62,12 @@ describe('create or update user phone use case', () => {
   });
 
   it('should createUserPhoneUseCase call your methods correctly', async () => {
+    const user_phone = {
+      phone_number: 'any_phone_number',
+      type: 'any_type',
+      id_user: user_mock.id,
+    };
+
     const findByIdSpy = jest
       .spyOn(userRepository, 'findById')
       .mockResolvedValueOnce(right(user_mock as any));
@@ -63,21 +75,23 @@ describe('create or update user phone use case', () => {
     const checkSpy = jest.spyOn(createUserPhoneParamsValidator, 'check');
     const findByUserIdSpy = jest.spyOn(userPhoneRepository, 'findByUserId');
 
-    await createUserPhoneUseCase.execute(user_phone_mock);
+    await createUserPhoneUseCase.execute(user_phone);
 
-    expect(findByIdSpy).toBeCalledWith(user_phone_mock.id_user);
-    expect(createOrUpdateSpy).toBeCalledWith(user_phone_mock);
-    expect(checkSpy).toBeCalledWith(user_phone_mock);
-    expect(findByUserIdSpy).toBeCalledWith(user_phone_mock.id_user);
+    expect(findByIdSpy).toBeCalledWith(user_phone.id_user);
+    expect(createOrUpdateSpy).toBeCalledWith(user_phone);
+    expect(checkSpy).toBeCalledWith(user_phone);
+    expect(findByUserIdSpy).toBeCalledWith(user_phone.id_user);
   });
 
   it('should createUserPhoneController call your methods correctly', async () => {
+    const user_phone = {
+      phone_number: 'any_phone_number',
+      type: 'any_type',
+      id_user: user_mock.id,
+    };
+
     const checkSpy = jest.spyOn(bodyRequestValidator, 'check');
     const executeSpy = jest.spyOn(createUserPhoneUseCase, 'execute');
-
-    const http_request = {
-      body: user_phone_mock,
-    };
 
     await createUserPhoneController.handle(http_request);
 
@@ -85,21 +99,17 @@ describe('create or update user phone use case', () => {
       body: user_phone_mock,
       fields: ['type', 'phone_number'],
     });
-    expect(executeSpy).toBeCalledWith(user_phone_mock);
+    expect(executeSpy).toBeCalledWith(user_phone);
   });
 
   it('should create a new user phone for a registered user', async () => {
-    const http_request = {
-      body: user_phone_mock,
-    };
-
     jest
       .spyOn(userRepository, 'findById')
       .mockResolvedValueOnce(right(user_mock as any));
 
     const http_response = await createUserPhoneController.handle(http_request);
 
-    expect(http_response.statusCode).toBe(200);
+    expect(http_response.statusCode).toBe(201);
     expect(http_response.body).toEqual(user_phone_mock);
   });
 
@@ -110,9 +120,6 @@ describe('create or update user phone use case', () => {
   });
 
   it('should not create a new user phone for a not registered user', async () => {
-    const http_request = {
-      body: user_phone_mock,
-    };
     const http_response = await createUserPhoneController.handle(http_request);
 
     expect(http_response).toEqual(badRequest(new UserNotFoundError()));
@@ -126,10 +133,6 @@ describe('create or update user phone use case', () => {
       .spyOn(userPhoneRepository, 'findByUserId')
       .mockResolvedValueOnce(right({} as any));
 
-    const http_request = {
-      body: user_phone_mock,
-    };
-
     const http_response = await createUserPhoneController.handle(http_request);
 
     expect(http_response).toEqual(badRequest(new UserAlreadyHavePhone()));
@@ -141,17 +144,28 @@ describe('create or update user phone use case', () => {
         ...user_phone_mock,
         phone_number: '123',
       },
+      params: {
+        id: user_mock.id,
+      },
     };
 
     const http_response = await createUserPhoneController.handle(http_request);
 
-    expect(http_response).toBe(200);
+    expect(http_response).toEqual(
+      badRequest(
+        new InvalidParamError(
+          'The param: phone_number have less than 9 characters',
+        ),
+      ),
+    );
   });
   it('should not accept a id_user with a invalid uuid format', async () => {
     const http_request = {
       body: {
         ...user_phone_mock,
-        id_user: 'invalid_id',
+      },
+      params: {
+        id: 'invalid_id',
       },
     };
 
@@ -162,5 +176,13 @@ describe('create or update user phone use case', () => {
         new InvalidParamError('The id_user does not have a valid uuid format'),
       ),
     );
+  });
+
+  it('should returns a server error if createUserPhoneUseCase throws an error ', async () => {
+    jest.spyOn(createUserPhoneUseCase, 'execute').mockRejectedValueOnce(new Error());
+
+    const http_response = await createUserPhoneController.handle(http_request);
+
+    expect(http_response).toEqual(serverError());
   });
 });
